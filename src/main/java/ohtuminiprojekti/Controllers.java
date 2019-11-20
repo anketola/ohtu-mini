6d7f802b1bfefa5ff1a8b0996e3113d3d5f0c45c
Antti Ketola <antti@anttiketola.com>
2019-11-20 11:18:27 +0200

    Tietokantaan tallennus ja haku

--- src/main/java/ohtuminiprojekti/Controllers.java
+++ src/main/java/ohtuminiprojekti/Controllers.java
@@ -1,12 +1,36 @@
 package ohtuminiprojekti;
 
+import ohtuminiprojekti.dao.BookRepository;
+import ohtuminiprojekti.domain.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Controllers {
 
+    @Autowired
+    private BookRepository bookRepository;
+    
+    @PostMapping("/books/create")
+    public String createBook(@RequestParam String title, @RequestParam String author) {
+        Book newBook = new Book();
+        newBook.setTitle(title);
+        newBook.setAuthor(author);
+        bookRepository.save(newBook);
+    return "redirect:/";
+    }
+    
+    @GetMapping("/books/list")
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+    return "listbooks";
+    }
+    
     @GetMapping("/")
     @ResponseBody
     public String indexRoot() {
