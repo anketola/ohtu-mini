74c5b13962c7f9f23a14e73fcee0e27e96356c8a
Antti Ketola <antti@anttiketola.com>
2019-11-24 08:53:01 +0200

    Lombok dependency to gradle

--- src/main/java/ohtuminiprojekti/domain/Book.java
+++ src/main/java/ohtuminiprojekti/domain/Book.java
@@ -2,43 +2,19 @@
 package ohtuminiprojekti.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book extends AbstractPersistable<Long> {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
   private String title;
   private String author;
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public void setAuthor(String author) {
-    this.author = author;
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public String getTitle() {
-    return this.title;
-  }
-
-  public String getAuthor() {
-    return this.author;
-  }
 
 }
