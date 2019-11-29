5bfe84f2c84c948f47622299d368a0ece395dcaf
Jeeses313 <jesse.vilkman@hotmail.fi>
2019-11-29 14:02:58 +0200

    Fix problem with switchin picture

--- src/main/java/ohtuminiprojekti/services/PictureService.java
+++ src/main/java/ohtuminiprojekti/services/PictureService.java
@@ -12,27 +12,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class PictureService {
-  
-  @Autowired
-  private PictureRepository pictureRepository;
-  
-  @Autowired BookRepository bookRepository;
-  
-  @Transactional
-  public void newPicture(MultipartFile file, String type, Long id) throws IOException {
-    if (file.getContentType().equals("image/jpeg")) {
-      if (type.equals("book")) {
-        Picture newPicture = new Picture();
-        newPicture.setContent(file.getBytes());
-        Book book = bookRepository.getOne(id);
-        book.setPicture(newPicture);
-        newPicture.setBook(book);
-      }
-    }
-  }
 
-  public byte[] getPicture(Long id) {
-    return pictureRepository.getOne(id).getContent();
-  }
+    @Autowired
+    private PictureRepository pictureRepository;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Transactional
+    public void newPicture(MultipartFile file, String type, Long id) throws IOException {
+        if (file.getContentType().equals("image/jpeg")) {
+            if (type.equals("book")) {
+                Book book = bookRepository.getOne(id);
+                if (book.getPicture() == null) {
+                    Picture newPicture = new Picture();
+                    newPicture.setContent(file.getBytes());
+                    book.setPicture(newPicture);
+                    newPicture.setBook(book);
+                } else {
+                    book.getPicture().setContent(file.getBytes());
+                }
+            }
+        }
+    }
+
+    public byte[] getPicture(Long id) {
+        return pictureRepository.getOne(id).getContent();
+    }
 
 }
