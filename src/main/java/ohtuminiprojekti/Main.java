f7ec55c1a7043d66b5ee915083046e715ec73e77
Antti Ketola <antti@anttiketola.com>
2019-12-06 10:11:39 +0200

    Static images for table

--- src/main/java/ohtuminiprojekti/Main.java
+++ src/main/java/ohtuminiprojekti/Main.java
@@ -3,6 +3,9 @@ package ohtuminiprojekti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -10,6 +13,17 @@ public class Main extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
+  }
+
+  @Configuration
+  public class WebConfig implements WebMvcConfigurer {      
+        
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/**")
+      .addResourceLocations("classpath:/static/")
+        .setCachePeriod(0);
+    }
   }
 
 }
